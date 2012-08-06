85e44147ce10c9e0eebc0b55d486dcfce3844aad
Marco Lisci <marco.lisci@badsharkco.com>
2012-08-06 14:15:47 +0200

    Fixed indentation using spaces

--- footer.php
+++ footer.php
@@ -3,9 +3,9 @@
 </footer>
 
 <!-- Scripts -->
-	<script src="<?php bloginfo('template_url'); ?>/js/libs/jquery.js"></script>
-	<script src="<?php bloginfo('template_url'); ?>/js/plugins.js"></script>
-	<script src="<?php bloginfo('template_url'); ?>/js/start.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/js/libs/jquery.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/js/plugins.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/js/start.js"></script>
 <!-- /Scripts -->
 
 <?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -5,19 +5,19 @@
 <!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
 
 <head>
-	<meta charset="utf-8">
-	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
-	
-	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/style.css" />
-	<script src="<?php bloginfo('template_url'); ?>/js/libs/modernizr.js"></script>
-		
-	<title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
+  <meta charset="utf-8">
+  <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
+  
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/style.css" />
+  <script src="<?php bloginfo('template_url'); ?>/js/libs/modernizr.js"></script>
+    
+  <title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
 
-	<?php wp_head(); ?>
+  <?php wp_head(); ?>
 </head>
 
 <body>
-	
-	<header>
-		
-	</header>
+  
+  <header>
+    
+  </header>
